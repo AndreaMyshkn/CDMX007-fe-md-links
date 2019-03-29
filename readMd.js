87c68543ec066aa58687f7c7c86d10393e79b03b
AndreaMyshkn <atrkvsk@hotmail.com>
2019-03-29 10:41:32 -0600

    Encuentra links en archivos con expresiones regulares

--- readMd.js
+++ readMd.js
@@ -1,32 +1,30 @@
-   const fs = require ('fs');
-    const file = ('./README.md')
-    const path = "./";
+   const fs = require('fs');
+   const file = ('./README.md')
+   const path = "./";
 
 
-    
-    fs.readFile(file,  (err, data)=>{
-      if(err){
-          console.log(err('ha ocurrido un error'))
-      } {
-          const convertToString = data.toString().cyan;
 
-        console.log(convertToString)
+   fs.readFile(file, (err, data) => {
+     if (err) {
+       console.log(err('ha ocurrido un error'))
+     } {
+       const convertToString = data.toString().cyan;
+       console.log(convertToString)
+       //Encuentra el número de coincidencias
+      //  const reg = RegExp('http', 'g');
+       const reg =  /\b(https?:\/\/.*?\.[a-z]{2,4}\b)/g;
+       const foundhttp = convertToString.match(reg);
+       console.log(foundhttp)
 
-      const reg = RegExp('http', 'g')
-      const foundhttp= convertToString.match(reg).length;
-      console.log(foundhttp)
- 
-      }})
-      
-  
-      //Leer todos los archivos del directorio
-  const readDir = fs.readdirSync(path);
- 
+     }
+   })
+
+
+   //Leer todos los archivos del directorio
+   const readDir = fs.readdirSync(path);
+
    console.log(readDir)
 
-    
-
-    //         const urlR = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
-//  const url= convertToString.match(urlR)
-        //   const foundLinksOnReadMe = convertToString.split("http").length-1;
-        //   console.log(foundLinksOnReadMe)
+  
+   //   const foundLinksOnReadMe = convertToString.split("http").length-1;
+   //   console.log(foundLinksOnReadMe)
